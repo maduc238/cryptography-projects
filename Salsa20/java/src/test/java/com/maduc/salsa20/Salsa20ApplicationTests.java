d816ffbdccce116602790ec222f38cc7547d262d
Ma Viet Duc <95759699+maduc238@users.noreply.github.com>
2022-11-05 11:02:39 +0700

    Add files via upload

--- Salsa20/java/src/test/java/com/maduc/salsa20/Salsa20ApplicationTests.java
+++ Salsa20/java/src/test/java/com/maduc/salsa20/Salsa20ApplicationTests.java
@@ -1,12 +1,7 @@
 package com.maduc.salsa20;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class Salsa20ApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
