b58853291179facba82ec7cea4cb565357608448
Ma Viet Duc <95759699+maduc238@users.noreply.github.com>
2022-11-25 12:31:59 +0700

    Add files via upload

---
+++ AES-128/Cipher.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define ROTL8(x,shift) ((uint8_t) ((x) << (shift)) | ((x) >> (8 - (shift))))
+#define Nk 4
+#define Nb 4
+#define Nr 10
+
+uint8_t *ByteToBinaryString(uint8_t byte) {
+    uint8_t mask = 1;
+    uint8_t *bits = (uint8_t *) malloc(8);
+    for (int i = 0; i < 8; i++)
+        bits[i] = (byte & (mask << i)) != 0;
+    return bits;
+}
+
+void PrintBinaryArray(uint8_t *bits) {
+    for (int i = 7; i >= 0; i--)
+        printf("%d",bits[i]);
+}
+
+uint8_t BinaryStringToByte(uint8_t* bits) {
+    uint8_t result = 0, mul = 1;
+    for (int i=0; i<8; i++) {
+        result += bits[i]*mul;
+        mul = mul << 1;
+    }
+    return result;
+}
+
+const int overflow = 0x100, modulus = 0x11B;
+uint8_t Multiply(int a, int b) {
+    uint8_t sum = 0;
+    while (b > 0) {
+        if (b & 1) sum = sum ^ a;
+        b = b >> 1;
+        a = a << 1;
+        if (a & overflow) a = a ^ modulus;
+    }
+    return sum;
+}
+
+void InitializeAESsbox(uint8_t sbox[256]) {
+	uint8_t p = 1, q = 1;
+	do {
+		p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
+		q ^= q << 1;
+		q ^= q << 2;
+		q ^= q << 4;
+		q ^= q & 0x80 ? 0x09 : 0;
+		uint8_t xformed = q ^ ROTL8(q, 1) ^ ROTL8(q, 2) ^ ROTL8(q, 3) ^ ROTL8(q, 4);
+		sbox[p] = xformed ^ 0x63;
+	} while (p != 1);
+
+	sbox[0] = 0x63;
+}
+
+void AddRoundKey(uint8_t data[16], uint8_t w[16]) {
+    for (int i=0; i<16; i++) {
+        data[i] ^= w[i];
+    }
+}
+
+void SubBytes(uint8_t data[16], uint8_t sbox[256]) {
+    for (int i=0; i<16; i++) {
+        data[i] = sbox[data[i]];
+    }
+}
+
+void ShiftRows(uint8_t data[16]) {
+    uint8_t temp;
+    temp = data[1]; data[1] = data[5]; data[5] = data[9]; data[9] = data[13]; data[13] = temp;
+    temp = data[2]; data[2] = data[10]; data[10] = temp; temp = data[6]; data[6] = data[14]; data[14] = temp;
+    temp = data[15]; data[15] = data[11]; data[11] = data[7]; data[7] = data[3]; data[3] = temp;
+}
+
+void MixColumns(uint8_t data[16]) {
+    uint8_t temp[16];
+    for (int i=0; i<4; i++) {
+        temp[4*i] = Multiply(2,data[4*i]) ^ Multiply(3,data[4*i+1]) ^ data[4*i+2] ^ data[4*i+3];
+        temp[4*i+1] = data[4*i] ^ Multiply(2,data[4*i+1]) ^ Multiply(3,data[4*i+2]) ^ data[4*i+3];
+        temp[4*i+2] = data[4*i] ^ data[4*i+1] ^ Multiply(2,data[4*i+2]) ^ Multiply(3,data[4*i+3]);
+        temp[4*i+3] = Multiply(3,data[4*i]) ^ data[4*i+1] ^ data[4*i+2] ^ Multiply(2,data[4*i+3]);
+    }
+    for (int i=0; i<16; i++)
+        data[i] = temp[i];
+}
+
+void RotWord(uint8_t data[4]) {
+    uint8_t temp = data[0]; data[0] = data[1]; data[1] = data[2]; data[2] = data[3]; data[3] = temp;
+}
+
+void *KeyExpansion(uint8_t key[16], uint8_t sbox[256]) {
+    static uint8_t w[16*(Nr+1)];
+    for (int i=0; i<16; i++)
+        w[i] = key[i];
+
+    uint8_t temp[4], temp2[4];
+    uint8_t Rcon[4] = {0x00, 0x00, 0x00, 0x00};
+    uint8_t RC[10] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36};
+    
+    temp[0] = w[12]; temp[1] = w[13]; temp[2] = w[14]; temp[3] = w[15];
+    int i = 0;
+    while (i < 4*Nr) {
+        if (i % 4 == 0) {
+            // printf("\n*******************************\n");
+            Rcon[0] = RC[i/4];
+            // printf("Rcon[0] %02x\n",Rcon[0]);
+            RotWord(temp);      // i chia het 4
+            for (int i=0; i<4; i++)
+                temp[i] = sbox[temp[i]] ^ Rcon[i];  // i chia het 4
+        }
+        temp2[0] = w[4*i]; temp2[1] = w[4*i+1]; temp2[2] = w[4*i+2]; temp2[3] = w[4*i+3];
+        for (int j=0; j<4; j++) {
+            temp[j] = temp[j] ^ temp2[j];
+            w[4*(i+4)+j] = temp[j];
+        }
+        i++;
+    }
+    return w;
+}
+
+int main() {
+    uint8_t input[16] = {0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34};
+    uint8_t key[16] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+    uint8_t sbox[256];
+    InitializeAESsbox(sbox);
+    uint8_t *w;
+    w = KeyExpansion(key, sbox);
+
+    AddRoundKey(input, key);
+    for (int i=1; i<=9; i++) {
+        SubBytes(input, sbox);
+        ShiftRows(input);
+        MixColumns(input);
+        for (int j=0; j<16; j++) {
+            key[j] = w[16*i+j];
+        }
+        AddRoundKey(input, key);
+    }
+    SubBytes(input, sbox);
+    ShiftRows(input);
+    for (int j=0; j<16; j++)
+        key[j] = w[16*Nr+j];
+    AddRoundKey(input, key);
+    
+    printf("Ciphertext\n");
+    for (int i=0; i<16; i++) {
+        printf("%02x ",input[i]);
+    }
+}
