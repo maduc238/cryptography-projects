1b5da7c912d162ab13f559f178c6ee015942079b
canhduc01 <81638699+canhduc01@users.noreply.github.com>
2022-12-29 08:14:29 +0700

    Create MaHoaBanTin.c

---
+++ AES-128/C/MaHoaBanTin.c
@@ -0,0 +1,77 @@
+/** Copyright (C) 2022 maduc238
+ *
+ * \file Cipher.c
+ *
+ * \author Ma Duc <mavietduc@gmail.com>
+ *
+ * Code mã hóa AES-128
+ * 
+ */
+
+#include "AES128.h"
+
+void Cipher(uint8_t input[], uint8_t key[], uint8_t sbox[]) {
+    printf("Plaintext:");
+    printf("\nHex: ");
+    for (int i=0; i<16; i++) {
+        printf("0x%02x,",input[i]);
+    }
+    printf("\nText: ");
+    for (int i=0; i<16; i++) {
+        printf("%c",input[i]);
+    }
+    printf("\n");
+    
+    // uint8_t sbox[256];      /* Dùng để tạo S-box */
+    // InitializeAESsbox(sbox);
+    uint8_t *w;             /* Dùng để tạo Key Expansion */
+    w = KeyExpansion(key, sbox);
+
+    AddRoundKey(input, key);
+    for (int i=1; i<=Nr-1; i++) {
+        SubBytes(input, sbox);
+        ShiftRows(input);
+        MixColumns(input);
+        for (int j=0; j<16; j++) {
+            key[j] = w[16*i+j];
+        }
+        AddRoundKey(input, key);
+    }
+    SubBytes(input, sbox);
+    ShiftRows(input);
+    for (int j=0; j<16; j++)
+        key[j] = w[16*Nr+j];
+    AddRoundKey(input, key);
+    
+    /* Print kết quả */
+    printf("\nCiphertext:");
+    printf("\nHex: ");
+    for (int i=0; i<16; i++) {
+        printf("0x%02x,",input[i]);
+    }
+    printf("\nText: ");
+    for (int i=0; i<16; i++) {
+        printf("%c",input[i]);
+    }
+
+	printf("\n*****************************************************\n\n");
+}
+
+int main(int argc, char *argv[]) {
+    uint8_t key[] = "xin chao cac ban";
+
+	FILE *fp;
+   	char buff[16];
+
+   	fp = fopen("E:/Download/AES-128/cryptography-projects-main/AES-128/C/plaintext.txt", "r");
+
+	uint8_t sbox[256];      /* Dùng để tạo S-box */
+    InitializeAESsbox(sbox);
+
+	while(fgets(buff, 17, (FILE*)fp) != NULL) {
+		uint8_t plaintext[16];
+		for (int i=0; i<16; i++)
+			plaintext[i] = (uint8_t)buff[i];
+		Cipher(buff, key, sbox);
+	}
+}
