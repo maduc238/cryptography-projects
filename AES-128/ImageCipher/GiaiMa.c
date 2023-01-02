59cd1caf26dec05824a7f7746e563d7b54882727
Ma Viet Duc <95759699+maduc238@users.noreply.github.com>
2023-01-02 10:55:15 +0700

    Add files via upload

--- AES-128/ImageCipher/GiaiMa.c
+++ AES-128/ImageCipher/GiaiMa.c
@@ -9,15 +9,11 @@ uint8_t toInt(char a){
 
 int main(int argc, char *argv[]) {
     // uint8_t key[] = {0xDE,0x8D,0xC9,0x1B,0xC4,0xD9,0x05,0x20,0xB4,0x66,0x25,0x2A,0xF0,0x22,0x24,0xB6};
-    printf("Running GiaiMa.c\n");
-    uint8_t key[] = "xin chao cac ban";
+    printf("Running GiaiMa.c ...\n");
+    uint8_t key[16];
     uint8_t key_temp[16];
-
-    char buff[32];
-    FILE *fp, *fp_write;
-
-    fp = fopen("data/encrypted.data", "r");
-    fp_write = fopen("data/picture.data", "w+");
+    for(int i=0; i<16; i++)
+        key[i] = (uint8_t) (toInt(argv[2][2*i])*16 + toInt(argv[2][2*i+1]));
 
     uint8_t sbox[256];      /* Dùng để tạo S-box */
     InitializeAESsbox(sbox);
@@ -32,46 +28,78 @@ int main(int argc, char *argv[]) {
         prev[i] = 0x00;
     }
 
-    while(fgets(buff, 33, (FILE*)fp) != NULL) {
-        sbox[0] = 0x63;
-        for (int i=0; i<16; i++)
-            plaintext[i] = (uint8_t) (toInt(buff[2*i])*16 + toInt(buff[2*i+1]));
-        
-        if(argv[1][0] == 'c')     // cbc
-            for (int i=0; i<16; i++)
-                plain[i] = plaintext[i];
+    char buff[32];
+    FILE *fp, *fp_write;
 
-        for (int j=0; j<16; j++)
-            key_temp[j] = w[16*Nr+j];
-        AddRoundKey(plaintext, key_temp);
+    fp = fopen("data/encrypted.data", "r");
+    fp_write = fopen("data/picture.data", "w+");
 
-        for (int i=Nr-1; i>=1; i--) {
+    if(argv[1][0] == 'e')          // ecb
+        while(fgets(buff, 33, (FILE*)fp) != NULL) {
+            sbox[0] = 0x63;
+            for (int i=0; i<16; i++) {
+                plaintext[i] = (uint8_t) (toInt(buff[2*i])*16 + toInt(buff[2*i+1]));
+                key_temp[i] = w[16*Nr+i];
+            }
+
+            AddRoundKey(plaintext, key_temp);
+
+            for (int i=Nr-1; i>=1; i--) {
+                InvShiftRows(plaintext);
+                InvSubBytes(plaintext, InvSBox);
+                for (int j=0; j<16; j++) {
+                    key_temp[j] = w[16*i+j];
+                }
+                AddRoundKey(plaintext, key_temp);
+                InvMixColumns(plaintext);
+            }
+
             InvShiftRows(plaintext);
             InvSubBytes(plaintext, InvSBox);
             for (int j=0; j<16; j++) {
-                key_temp[j] = w[16*i+j];
+                key_temp[j] = w[j];
             }
             AddRoundKey(plaintext, key_temp);
-            InvMixColumns(plaintext);
-        }
 
-        InvShiftRows(plaintext);
-        InvSubBytes(plaintext, InvSBox);
-        for (int j=0; j<16; j++) {
-            key_temp[j] = w[j];
-        }
-        AddRoundKey(plaintext, key_temp);
-
-        // Write to file
-        if(argv[1][0] == 'e')          // ecb
+            // Write to file
             for (int i=0; i<16; i++)
                 fprintf(fp_write,"%02x",plaintext[i]);
-        else if(argv[1][0] == 'c')     // cbc
+        }
+
+    else if(argv[1][0] == 'c')     // cbc
+        while(fgets(buff, 33, (FILE*)fp) != NULL) {
+            sbox[0] = 0x63;
+            for (int i=0; i<16; i++){
+                plaintext[i] = (uint8_t) (toInt(buff[2*i])*16 + toInt(buff[2*i+1]));
+                plain[i] = plaintext[i];
+                key_temp[i] = w[16*Nr+i];
+            }
+
+            AddRoundKey(plaintext, key_temp);
+
+            for (int i=Nr-1; i>=1; i--) {
+                InvShiftRows(plaintext);
+                InvSubBytes(plaintext, InvSBox);
+                for (int j=0; j<16; j++) {
+                    key_temp[j] = w[16*i+j];
+                }
+                AddRoundKey(plaintext, key_temp);
+                InvMixColumns(plaintext);
+            }
+
+            InvShiftRows(plaintext);
+            InvSubBytes(plaintext, InvSBox);
+            for (int j=0; j<16; j++) {
+                key_temp[j] = w[j];
+            }
+            AddRoundKey(plaintext, key_temp);
+
+            // Write to file
             for (int i=0; i<16; i++){
                 fprintf(fp_write,"%02x",plaintext[i]^prev[i]);
                 prev[i] = plain[i];
             }
-	}
+        }
 
     fclose(fp);
     fclose(fp_write);
--- AES-128/ImageCipher/MaHoa.c
+++ AES-128/ImageCipher/MaHoa.c
@@ -9,15 +9,11 @@ uint8_t toInt(char a){
 
 int main(int argc, char *argv[]) {
     // uint8_t key[] = {0xDE,0x8D,0xC9,0x1B,0xC4,0xD9,0x05,0x20,0xB4,0x66,0x25,0x2A,0xF0,0x22,0x24,0xB6};
-    printf("Running MaHoa.c\n");
-    uint8_t key[] = "xin chao cac ban";
+    printf("Running MaHoa.c ...\n");
+    uint8_t key[16];
     uint8_t key_temp[16];
-
-    char buff[32];
-    FILE *fp, *fp_write;
-
-    fp = fopen("data/picture.data", "r");
-    fp_write = fopen("data/encrypted.data", "w+");
+    for(int i=0; i<16; i++)
+        key[i] = (uint8_t) (toInt(argv[2][2*i])*16 + toInt(argv[2][2*i+1]));
 
     uint8_t sbox[256];      /* Dùng để tạo S-box */
     InitializeAESsbox(sbox);
@@ -27,41 +23,71 @@ int main(int argc, char *argv[]) {
     for (int i=0; i<16; i++)
         plaintext[i] = 0x00;
 
-    while(fgets(buff, 33, (FILE*)fp) != NULL) {
-        sbox[0] = 0x63;
-        for (int j=0; j<16; j++) {
-            key_temp[j] = w[j];
-        }
-		
-        if(argv[1][0] == 'e')          // ecb
-            for (int i=0; i<16; i++)
-                plaintext[i] = (uint8_t) (toInt(buff[2*i])*16 + toInt(buff[2*i+1]));
-        
-        else if(argv[1][0] == 'c')     // cbc
-            for (int i=0; i<16; i++)
-                plaintext[i] ^= (uint8_t) (toInt(buff[2*i])*16 + toInt(buff[2*i+1]));
+    char buff[32];
+    FILE *fp, *fp_write;
 
-        AddRoundKey(plaintext, key_temp);
-        for (int i=1; i<=Nr-1; i++) {
+    fp = fopen("data/picture.data", "r");
+    fp_write = fopen("data/encrypted.data", "w+");
+
+    if(argv[1][0] == 'e')          // ecb
+        while(fgets(buff, 33, (FILE*)fp) != NULL) {
+            sbox[0] = 0x63;
+            for (int i=0; i<16; i++) {
+                key_temp[i] = w[i];
+                plaintext[i] = (uint8_t) (toInt(buff[2*i])*16 + toInt(buff[2*i+1]));
+            }
+
+            AddRoundKey(plaintext, key_temp);
+            for (int i=1; i<=Nr-1; i++) {
+                SubBytes(plaintext, sbox);
+                ShiftRows(plaintext);
+                MixColumns(plaintext);
+                for (int j=0; j<16; j++) {
+                    key_temp[j] = w[16*i+j];
+                }
+                AddRoundKey(plaintext, key_temp);
+            }
             SubBytes(plaintext, sbox);
             ShiftRows(plaintext);
-            MixColumns(plaintext);
-            for (int j=0; j<16; j++) {
-                key_temp[j] = w[16*i+j];
-            }
+            for (int j=0; j<16; j++)
+                key_temp[j] = w[16*Nr+j];
             AddRoundKey(plaintext, key_temp);
-        }
-        SubBytes(plaintext, sbox);
-        ShiftRows(plaintext);
-        for (int j=0; j<16; j++)
-            key_temp[j] = w[16*Nr+j];
-        AddRoundKey(plaintext, key_temp);
 
-        // Write to file
-        for (int i=0; i<16; i++) {
-            fprintf(fp_write,"%02x",plaintext[i]);
+            // Write to file
+            for (int i=0; i<16; i++) {
+                fprintf(fp_write,"%02x",plaintext[i]);
+            }
         }
-	}
+
+    else if(argv[1][0] == 'c')     // cbc
+        while(fgets(buff, 33, (FILE*)fp) != NULL) {
+            sbox[0] = 0x63;
+            for (int i=0; i<16; i++) {
+                key_temp[i] = w[i];
+                plaintext[i] ^= (uint8_t) (toInt(buff[2*i])*16 + toInt(buff[2*i+1]));
+            }
+
+            AddRoundKey(plaintext, key_temp);
+            for (int i=1; i<=Nr-1; i++) {
+                SubBytes(plaintext, sbox);
+                ShiftRows(plaintext);
+                MixColumns(plaintext);
+                for (int j=0; j<16; j++) {
+                    key_temp[j] = w[16*i+j];
+                }
+                AddRoundKey(plaintext, key_temp);
+            }
+            SubBytes(plaintext, sbox);
+            ShiftRows(plaintext);
+            for (int j=0; j<16; j++)
+                key_temp[j] = w[16*Nr+j];
+            AddRoundKey(plaintext, key_temp);
+
+            // Write to file
+            for (int i=0; i<16; i++) {
+                fprintf(fp_write,"%02x",plaintext[i]);
+            }
+        }
 
     fclose(fp);
     fclose(fp_write);
